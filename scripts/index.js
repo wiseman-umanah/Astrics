1a2aca51cc5c7f531d71b184603286966b6ebc5a
wiseman-umanah <wisemanumanah@gmail.com>
2024-04-23 23:03:52 +0100

    Added login path for registration of users

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -18,22 +19,26 @@ appId: "1:320016531095:web:63333aba43cdd238f7f3d2"
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+const db = getDatabase();
+const auth = getAuth(app);
 
 // grab button
-const submit = document.getElementById("submit-btn")
+const submit = document.getElementById("submit-btn");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
+const MainForm = document.getElementById('submit-btn');
 
 //send data to firebase
-submit.addEventListener("click", function(event){
+let RegisterUSer = event => {
 	//grab inputs
-	const email = document.getElementById("email").value;
-	const password = document.getElementById("password").value;
-	event.preventDefault()
-	createUserWithEmailAndPassword(auth, email, password)
-		.then((userCredential) => {
+	event.preventDefault();
+	createUserWithEmailAndPassword(auth, email.value, password.value)
+	.then((userCredential) => {
     // Signed up 
-    const user = userCredential.user;
-	window.location.href = "index.html"
+	    const user = userCredential.user;
+		window.location.href = "index.html"
     // ...
   })
   .catch((error) => {
@@ -41,4 +46,6 @@ submit.addEventListener("click", function(event){
     const errorMessage = error.message;
     // ..
   });
-})
+}
+
+MainForm.addEventListener('click', RegisterUSer);
