98b9e958fd441f0dba40919f07afeaa6ed8dd000
wiseman-umanah <wisemanumanah@gmail.com>
2024-04-25 20:39:34 +0100

    Fixed up setup for database

--- setup.sql
+++ setup.sql
@@ -1,4 +1,6 @@
 -- script to setup database for deployment
+
 CREATE DATABASE IF NOT EXISTS Astrics;
-CREATE USER IF NOT EXISTS astrics IDENTIFIED BY astrics_pwd;
-GRANT ALL PRIVILEGES ON Astrics.* TO 'astrics'@'localhost'
+CREATE USER IF NOT EXISTS 'astrics'@'localhost' IDENTIFIED BY "astrics_pwd";
+GRANT ALL PRIVILEGES ON Astrics.* TO 'astrics'@'localhost';
+FLUSH PRIVILEGES;
