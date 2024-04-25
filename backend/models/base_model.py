711e4f977a2317c656202f2a800c37dd02fae632
wiseman-umanah <wisemanumanah@gmail.com>
2024-04-25 03:42:34 +0100

    Setup daatabase management £

--- backend/models/base_model.py
+++ backend/models/base_model.py
@@ -8,60 +8,64 @@ from os import getenv
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
-# if getenv("STORAGE MET") == "db":
-#     Base = declarative_base()
-# else:
-#     Base = object
+if getenv("STORAGE MET") == "db":
+	Base = declarative_base()
+else:
+	Base = object
 
 class BaseModel:
-    """A base class for all hbnb models"""
+	"""A base class for all hbnb models"""
+	if getenv("STORAGE MET") == "db":
+		id = Column(String(60), primary_key=True)
+		created_at = Column(DateTime, default=datetime.utcnow)
+		updated_at = Column(DateTime, default=datetime.utcnow)
+		
+	def __init__(self, *args, **kwargs):
+		"""Instatntiates a new model"""
+		if kwargs:
+			for key, value in kwargs.items():
+				if key != "__class__":
+					setattr(self, key, value)
+			if kwargs.get("created_at", None) and type(self.created_at) is str:
+				self.created_at = datetime.strptime(kwargs["created_at"], time)
+			else:
+				self.created_at = datetime.utcnow()
+			if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+				self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+			else:
+				self.updated_at = datetime.utcnow()
+			if kwargs.get("id", None) is None:
+				self.id = str(uuid.uuid4())
+		else:
+			self.id = str(uuid.uuid4())
+			self.created_at = datetime.utcnow()
+			self.updated_at = self.created_at
 
-    def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            if kwargs.get("created_at", None) and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], time)
-            else:
-                self.created_at = datetime.utcnow()
-            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
-            else:
-                self.updated_at = datetime.utcnow()
-            if kwargs.get("id", None) is None:
-                self.id = str(uuid.uuid4())
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = self.created_at
+	def __str__(self):
+		"""Returns a string representation of the instance"""
+		cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+		return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
-    def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+	def save(self):
+		"""Updates updated_at with current time when instance is changed"""
+		from backend.models import storage
+		self.updated_at = datetime.now()
+		storage.new(self)
+		storage.save()
 
-    def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        from backend.models import storage
-        self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
+	def to_dict(self):
+		"""Convert instance into dict format"""
+		dictionary = {}
+		dictionary.update(self.__dict__)
+		dictionary.update({'__class__':
+						  (str(type(self)).split('.')[-1]).split('\'')[0]})
+		dictionary['created_at'] = self.created_at.isoformat()
+		dictionary['updated_at'] = self.updated_at.isoformat()
+		if "_sa_instance_state" in dictionary:
+			del dictionary["_sa_instance_state"]
+		return dictionary
 
-    def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        if "_sa_instance_state" in dictionary:
-            del dictionary["_sa_instance_state"]
-        return dictionary
-
-    def delete(self):
-        """Delete the current instance from the storage"""
-        from models import storage
-        storage.delete(self)
+	def delete(self):
+		"""Delete the current instance from the storage"""
+		from models import storage
+		storage.delete(self)
---
+++ backend/models/engine/db_storage.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+"""
+Contains the class DBStorage
+"""
+
+import backend.models
+from backend.models.base_model import Base
+from backend.models.user import User
+from backend.models.image import Image
+from os import getenv
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+classes = {"Image": Image, "User": User}
+
+
+class DBStorage:
+    """interaacts with the MySQL database"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Instantiate a DBStorage object"""
+        USER = getenv('USER')
+        PWD = getenv('PWD')
+        HOST = getenv('HOST')
+        STORAGE = getenv('STORAGE_MET')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                      format(USER,
+                                             PWD,
+                                             HOST,
+                                             STORAGE))
+
+    def all(self, cls=None):
+        """query on the current database session"""
+        new_dict = {}
+        for clss in classes:
+            if cls is None or cls is classes[clss] or cls is clss:
+                objs = self.__session.query(classes[clss]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    new_dict[key] = obj
+        return (new_dict)
+
+    def new(self, obj):
+        """add the object to the current database session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """commit all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """delete from the current database session obj if not None"""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """reloads data from the database"""
+        Base.metadata.create_all(self.__engine)
+        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sess_factory)
+        self.__session = Session
+
+    def close(self):
+        """call remove() method on the private session attribute"""
+        self.__session.remove()
+
+    def get(self, cls, id):
+        """
+        Returns the object based on the class name and its ID, or
+        None if not found
+        """
+        if cls not in classes.values():
+            return None
+
+        all_cls = backend.models.storage.all(cls)
+        for value in all_cls.values():
+            if (value.id == id):
+                return value
+
+        return None
+
+    def count(self, cls=None):
+        """
+        count the number of objects in storage
+        """
+        all_class = classes.values()
+
+        if not cls:
+            count = 0
+            for clas in all_class:
+                count += len(backend.models.storage.all(clas).values())
+        else:
+            count = len(backend.models.storage.all(cls).values())
+
+        return count
