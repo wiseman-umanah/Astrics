77fa52646bd4a55f184e9569ff1f07fdaca20e23
wiseman-umanah <wisemanumanah@gmail.com>
2024-04-25 09:33:58 +0100

    Added user setup for database

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from backend.models.base_model import BaseModel
+from backend.models.base_model import BaseModel, Base
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
 	"""This class defines a user by various attributes"""
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
---
+++ setup.sql
@@ -0,0 +1,4 @@
+-- script to setup database for deployment
+CREATE DATABASE IF NOT EXISTS Astrics;
+CREATE USER IF NOT EXISTS astrics IDENTIFIED BY astrics_pwd;
+GRANT ALL PRIVILEGES ON Astrics.* TO 'astrics'@'localhost'
