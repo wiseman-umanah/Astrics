5ecd21f4af7fc0adf061b4149f351c30af1cbf4d
wiseman-umanah <wisemanumanah@gmail.com>
2024-05-01 20:58:13 +0100

    Addded functionality to protect users password in database

--- backend/models/user.py
+++ backend/models/user.py
@@ -5,6 +5,7 @@ from os import getenv
 from sqlalchemy import Column, String, Boolean, DateTime
 from dotenv import load_dotenv
 from datetime import datetime
+from hashlib import md5
 
 
 load_dotenv()
@@ -16,7 +17,7 @@ class User(BaseModel, Base):
 		__tablename__ = "users"
 		id = Column(String(60), primary_key=True)
 		email = Column(String(500), nullable=False)
-		password = Column(String(30), nullable=False)
+		password = Column(String(1024), nullable=False)
 		first_name = Column(String(100), nullable=False)
 		last_name = Column(String(100), nullable=False)
 		created_at = Column(DateTime, default=datetime.utcnow)
@@ -30,3 +31,8 @@ class User(BaseModel, Base):
 			self.last_name = ""
 			self.subscribed = True
 
+	def __setattr__(self, name, value):
+		"""sets a password with md5 encryption"""
+		if name == "password":
+			value = md5(value.encode()).hexdigest()
+		super().__setattr__(name, value)
