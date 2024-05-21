459b41a0407efb22d83d94fcfff752d38336d0c3
wiseman-umanah <wisemanumanah@gmail.com>
2024-05-21 13:38:41 +0000

    Fixed up home page for all screens across

--- frontend/webApp.py
+++ frontend/webApp.py
@@ -132,4 +132,4 @@ def astrics_register():
 
 if __name__ == "__main__":
 	""" Main Function """
-	app.run()
+	app.run(host="0.0.0.0")
--- setup.sql
+++ setup.sql
@@ -1,6 +1,3 @@
 -- script to setup database for deployment
 
 CREATE DATABASE IF NOT EXISTS Astrics;
-CREATE USER IF NOT EXISTS 'astrics'@'localhost' IDENTIFIED BY "astrics_pwd";
-GRANT ALL PRIVILEGES ON Astrics.* TO 'astrics'@'localhost';
-FLUSH PRIVILEGES;
