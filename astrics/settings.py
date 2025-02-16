b20493dbf860e122c7747afd1f38ac23ce396fe8
wiseman-umanah <wisemanumanah@gmail.com>
2025-02-16 22:17:08 +0100

    Setting up sitemap on webapp

--- astrics/settings.py
+++ astrics/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'django.contrib.sites',
+	'django.contrib.sitemaps',
 	'space',
 	'system',
 	'social_django',
