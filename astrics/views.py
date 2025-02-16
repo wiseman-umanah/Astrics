6f8915cbbc15be8bafa05a442e6bc71117a704be
wiseman-umanah <wisemanumanah@gmail.com>
2025-02-16 17:12:11 +0100

    Changed landing page hero to view on the latest content of the day

--- astrics/views.py
+++ astrics/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from system.models import AstricsModel
 
 
 def landing_page(request):
-	return render(request, 'account/landing-page.html')
+	latest_post = AstricsModel.objects.first()
+	return render(request, 'account/landing-page.html',
+			   {
+				   'latest_post': latest_post
+			   })
