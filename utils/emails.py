8a8b156300d18b6b7a90a10541adac7cf529f211
wiseman-umanah <wisemanumanah@gmail.com>
2025-02-16 16:50:50 +0100

    Fixed daily update email sending to users and admins on success and failure

--- utils/emails.py
+++ utils/emails.py
@@ -2,16 +2,29 @@
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.contrib.auth.models import User
+import random
+
+
+email_subjects = [
+    "New Space Content: Explore the Latest Discovery",
+    "Unveiling New Space Content: What’s Out There?",
+    "Discover New Space Content: Insights and Innovations",
+    "New Space Content Alert: Don’t Miss Out!",
+    "Dive into New Space Content: Expanding Our Universe",
+    "New Space Content: Fascinating Facts and Figures",
+    "Get Excited for New Space Content: Upcoming Releases",
+    "New Space Content: Join the Journey Beyond Earth"
+]
 
 
 def notify_user_new_content(content_data):
 	users  = User.objects.all()
 
-	subject = "Wiseman from Astrics"
+	subject = random.choice(email_subjects)
 
 	for user in users:
 		html_message  = render_to_string(
-			'email/daily_update.html',
+			'emails/daily_update.html',
 			{
 				'content': content_data,
 				'username': user.username
@@ -23,11 +36,11 @@ def notify_user_new_content(content_data):
 
 
 def notify_admins(message):
-	admins = User.objects.filter(is_admin=True).values_list('emails', flat=True)
+	admins = User.objects.filter(is_superuser=True).values_list('email', flat=True)
 
-	subject = "Wiseman from Astrics"
+	subject = "Important Notice"
 	html_message = render_to_string(
-		'email/admin_failure.html',
+		'emails/admin_failure.html',
 		{
 			'message': message
 		}
