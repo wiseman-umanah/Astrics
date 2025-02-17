ca526dc37d572780b20e383913ec9fda568f5bb6
wiseman-umanah <wisemanumanah@gmail.com>
2025-02-17 17:22:24 +0100

    Removed debug mode and remove input for collecting static files

--- astrics/settings.py
+++ astrics/settings.py
@@ -28,9 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
 
 
 # Application definition
--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 set -o errexit
 
 # collect all static assets and resources
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 
 # start Django server
 gunicorn astrics.wsgi:application --bind 0.0.0.0:8000 &
